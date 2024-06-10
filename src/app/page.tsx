a14a0f126cdf713e5bcd2ce1ad338296117243c8
Mauricio Andreolla de Brito <mauricioandreolla0000@gmail.com>
2024-06-10 19:03:40 -0300

    Melhorias na interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Loading from './components/Loading';
-import Currencies from './data/currencies.json';
+import Currencies from './data/db.json';
 import { toast, ToastContainer } from 'react-toastify';
+import { ArrowRightIcon } from 'lucide-react';
+import { Currency } from './types/Types';
 
 export default function Home() {
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+  const baseURLIcons = process.env.NEXT_PUBLIC_BASE_URL_ICONS;
 
   const [valueHigh, setValueHigh] = useState(1);
   const [valueLow, setValueLow] = useState(1);
@@ -18,13 +22,25 @@ export default function Home() {
 
   const [currency, setCurrency] = useState([{
     label: '',
-    name: ''
+    name: '',
+    codeCountry: ''
   }]);
+
+  const [icoCurrency, setIcoCurrency] = useState<Currency>({
+    code: 'USD',
+    CodeCountry: 'US',
+    name: 'Dolár Americano'
+  });
+
+  const [icoCurrencySecond, setIcoCurrencySecond] = useState<Currency>({
+    code: 'BRL',
+    CodeCountry: 'BR',
+    name: 'Real Brasileiro'
+  });
 
   const [primaryCurrency, setPrimaryCurrency] = useState('USD');
   const [secondCurrency, setSecondCurrency] = useState('BRL');
 
-  const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
   function GetValues() {
     const endpoint = `/json/last/${primaryCurrency}-${secondCurrency}`;
@@ -67,7 +83,9 @@ export default function Home() {
   }
 
   const converNumberToMonetary = function (value: number) {
-    return Intl.NumberFormat("pt-BR", {
+
+
+    return Intl.NumberFormat(navigator.language, {
       style: 'currency',
       maximumFractionDigits: 2,
       currencySign: 'standard',
@@ -80,7 +98,8 @@ export default function Home() {
     let values = Currencies?.data.map(currency => {
       return {
         label: currency.code,
-        name: currency.name
+        name: currency.name,
+        codeCountry: currency.code_country
       }
     });
 
@@ -101,8 +120,30 @@ export default function Home() {
       }
   }
 
+  const setIcoCurrencyValue = () => {
+    const value = currency.find(x => x.label == primaryCurrency);
+
+    setIcoCurrency({
+      code: value?.label ?? 'USD',
+      CodeCountry: value?.codeCountry ?? 'US',
+      name: value?.name ?? 'Dolár Americano'
+    });
+  }
+
+  const setIconCorrencySecondValue = () => {
+    const value = currency.find(x => x.label == secondCurrency);
+
+    setIcoCurrencySecond({
+      code: value?.label ?? 'BRL',
+      CodeCountry: value?.codeCountry ?? 'BR',
+      name: value?.name ?? 'Real Brasileiro'
+    });
+  }
+
   useEffect(() => {
     GetValues()
+    setIcoCurrencyValue()
+    setIconCorrencySecondValue()
 
   }, [primaryCurrency, secondCurrency])
 
@@ -123,17 +164,17 @@ export default function Home() {
 
           :
           <>
-            <div className="flex flex-col items-center justify-center w-[550px] h-[450px] rounded bg-zinc-900 hover:shadow-xl hover:shadow-green-900/50 transition duration-500 delay-400 mt-5">
+            <div className="flex flex-col items-center justify-center w-[550px] h-[470px] rounded bg-zinc-900 hover:shadow-xl hover:shadow-green-900/50 transition duration-500 delay-400 mt-5">
 
-              <div className='h-screen w-full'>
+              <div className='h-full w-full'>
 
-                <div className="mt-3 text-center">
+                <div className="mt-5 text-center">
                   <h5 className="text-white text-center font-bold text-2xl">Conversão de moedas para <span className='font-bold text-green-700'>{primaryCurrency}</span></h5>
                 </div>
 
                 <div className="flex flex-col justify-center items-center mt-3">
 
-                  <label className="text-left my-3">Moeda da cotação <strong>(De)</strong></label>
+                  <label className="text-left my-2">Moeda da cotação <strong>(De)</strong></label>
 
                   <select className='rounded-lg p-3 hover:opacity-95 text-zinc-900 outline-none' name='currency' id='currency' onChange={(e) => setPrimaryCurrency(e.target.value)} value={primaryCurrency}>
                     {
@@ -163,21 +204,60 @@ export default function Home() {
                   </select>
                 </div>
 
-                <div className="flex flex-col justify-center items-center mt-3">
+                <div className="flex flex-row justify-center items-center mt-3">
 
-                  <label className="text-left my-3 ">Valor em <span className='font-bold text-green-700'>{primaryCurrency}</span></label>
+                  <div className='flex flex-col justify-center items-center'>
+                    <div className='flex flex-row justify-center items-center'>
+                      <label className="text-left my-3 ">Valor em <span className='font-bold text-red-700'>{primaryCurrency}</span></label>
 
-                  <input type="number"
-                    className='rounded-lg p-3 hover:opacity-95 text-zinc-900 outline-none'
-                    id='value'
-                    value={inputValue || ''}
-                    onChange={(e) => setInputValue(+e.target.value)}
-                  />
+                      <img
+                        id='CodeCountry'
+                        className='mx-2 rounded-full hover:animate-ping'
+                        width={30}
+                        height={10}
+                        alt={`${icoCurrency?.name} ?? Not Found`}
+                        src={`${baseURLIcons}/country-flag-icons/3x2/${icoCurrency?.CodeCountry}.svg` ?? ''} />
+
+                    </div>
+
+                    <input type="number"
+                      className='rounded-lg p-3 hover:opacity-95 text-zinc-900 outline-none'
+                      id='value'
+                      value={inputValue || ''}
+                      onChange={(e) => setInputValue(+e.target.value)}
+                    />
+
+                  </div>
 
                 </div>
 
-                <div className="flex justify-center items-center mt-3 p-2">
-                  <button className='w-[100px] h-[40px] bg-green-800 hover:bg-green-700 rounded-lg mx-2' onClick={() => CalcValues()}>Calcular</button>
+                <div className='m-2 flex justify-center items-center'>
+                  <div className='flex flex-row justify-center items-center'>
+
+
+                    <img
+                      id='CodeCountry'
+                      className='m-2'
+                      width={30}
+                      height={10}
+                      alt={`${icoCurrency?.name} ?? Not Found`}
+                      src={`${baseURLIcons}/country-flag-icons/3x2/${icoCurrency?.CodeCountry}.svg` ?? ''} />
+
+                    <ArrowRightIcon className='mx-2' />
+
+                    <img
+                      id='CodeCountrySecond'
+                      className='m-2'
+                      width={30}
+                      height={10}
+                      alt={`${icoCurrencySecond?.name} ?? Not Found`}
+                      src={`${baseURLIcons}/country-flag-icons/3x2/${icoCurrencySecond?.CodeCountry}.svg` ?? ''} />
+
+                  </div>
+                </div>
+
+                <div className="flex justify-center items-center">
+                  <button className='w-[100px] h-[40px] bg-green-800 hover:bg-green-700 rounded-lg' onClick={() => CalcValues()}>Calcular</button>
                 </div>
 
               </div>
--- src/app/types/Types.tsx
+++ src/app/types/Types.tsx
@@ -1,5 +1,12 @@
 export interface CardType {
-    title: string,
-    url?: string,
-    content?: string
-  }
+  title: string,
+  url?: string,
+  content?: string
+}
+
+export interface Currency {
+  code: string,
+  name: string,
+  CodeCountry: string,
+}
+
